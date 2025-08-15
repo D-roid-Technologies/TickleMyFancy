9a56589b311852b187c35163c3b42f9193fa5ad7
stellanechi <stellanechi65@gmail.com>
2025-08-15 10:50:49 +0100

    updated features page

--- src/app/ui/AppEntry.tsx
+++ src/app/ui/AppEntry.tsx
@@ -3,13 +3,15 @@ import React from "react";
 import Index from "../routes/Index";
 import Layout from "./components/Layout";
 import SubscriptionCard from "./components/Subscription/Subscription";
+import Hero from "./components/Hero";
 
 const AppEntry: React.FC = () => {
   return (
     <>
       <Layout>
         <Index />
-       <SubscriptionCard/>
+        <Hero />
+        <SubscriptionCard />
       </Layout>
     </>
   );
